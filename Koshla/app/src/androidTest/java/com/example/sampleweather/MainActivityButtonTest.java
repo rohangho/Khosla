3ee2c52d55a7de9b3f340d2be5dd0447c5cbd966
rohanghosh <rohangho@live.com>
2020-02-15 21:26:02 +0530

    Added Lottie file

--- Koshla/app/src/androidTest/java/com/example/sampleweather/MainActivityButtonTest.java
+++ Koshla/app/src/androidTest/java/com/example/sampleweather/MainActivityButtonTest.java
@@ -1,18 +1,36 @@
 package com.example.sampleweather;
 
-import android.support.test.runner.AndroidJUnit4;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
-import com.example.sampleweather.activity.MainActivity;
+import com.example.sampleweather.activity.WeatherActivity;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
-class MainActiTvityButtonTest {
+@LargeTest
+public class MainActivityButtonTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mainActivityActivityTesting =
-            new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<WeatherActivity> activityRule =
+            new ActivityTestRule<>(WeatherActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        Espresso.onView(withId(R.id.showMore)).perform(click());
+        Espresso.onView(withText(R.id.recycler)).check(matches(isDisplayed()));
+
+    }
+
 }
--- Koshla/app/src/main/java/com/example/sampleweather/activity/WeatherActivity.java
+++ Koshla/app/src/main/java/com/example/sampleweather/activity/WeatherActivity.java
@@ -13,6 +13,7 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.bumptech.glide.Glide;
 import com.example.sampleweather.R;
 import com.example.sampleweather.adapter.BottomAdapter;
@@ -42,6 +43,7 @@ public class WeatherActivity extends AppCompatActivity {
     private AppCompatImageView iconTemperature;
     private AppCompatButton showMore;
     private List<BottomTemp> alltemp = new ArrayList<>();
+    private LottieAnimationView lottieAnimationView;
 
 
     @Override
@@ -54,12 +56,17 @@ public class WeatherActivity extends AppCompatActivity {
         temperature = findViewById(R.id.appCompatTextView);
         date = findViewById(R.id.date);
         showMore = findViewById(R.id.showMore);
+        lottieAnimationView = findViewById(R.id.animation_view);
 
-        MyViewModel factory = new MyViewModel(this.getApplication(), Integer.toString(ids));
-        weatherViewModel = ViewModelProviders.of(this, factory).get(WeatherViewModel.class);
-        weatherViewModel.init();
-        weatherViewModel.getWeatherRepo().observe(this, this::getResponsFromLiveData);
+        lottieAnimationView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lottieAnimationView.setVisibility(View.GONE);
+                retryCalling();
+            }
+        });
 
+        retryCalling();
         showMore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,23 +86,37 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     /**
+     * Retry calling for weather
+     */
+    private void retryCalling() {
+        MyViewModel factory = new MyViewModel(this.getApplication(), Integer.toString(ids));
+        weatherViewModel = ViewModelProviders.of(this, factory).get(WeatherViewModel.class);
+        weatherViewModel.init();
+        weatherViewModel.getWeatherRepo().observe(this, this::getResponsFromLiveData);
+
+    }
+    /**
      * This function
      *
      * @param baseResponse
      */
     private void getResponsFromLiveData(BaseResponse baseResponse) {
-        Glide.with(this).load("http://openweathermap.org/img/wn/" + baseResponse.getList().get(0).getWeather().get(0).getIcon() + "@2x" + ".png").into(iconTemperature);
-        temperature.setText(new DecimalFormat("##.##").format(baseResponse.getList().get(0).getMain().getTemp() - 273.15) + " \u2103");
-        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        SimpleDateFormat sdf2 = new SimpleDateFormat("dd-MMM-yyyy");
-        try {
-            String ds2 = sdf2.format(sdf1.parse(baseResponse.getList().get(0).getDtTxt()));
-            date.setText(ds2);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        if (baseResponse != null) {
+            Glide.with(this).load("http://openweathermap.org/img/wn/" + baseResponse.getList().get(0).getWeather().get(0).getIcon() + "@2x" + ".png").into(iconTemperature);
+            temperature.setText(new DecimalFormat("##.##").format(baseResponse.getList().get(0).getMain().getTemp() - 273.15) + " \u2103");
+            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            SimpleDateFormat sdf2 = new SimpleDateFormat("dd-MMM-yyyy");
+            try {
+                String ds2 = sdf2.format(sdf1.parse(baseResponse.getList().get(0).getDtTxt()));
+                date.setText(ds2);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
 
-        getFiveDays(baseResponse);
+            getFiveDays(baseResponse);
+        } else {
+            lottieAnimationView.setVisibility(View.VISIBLE);
+        }
 
 
     }
@@ -113,7 +134,7 @@ public class WeatherActivity extends AppCompatActivity {
                                 new DecimalFormat("##.##").format(baseResponse.getList().get(i).getMain().getTemp() - 273.15),
                                 baseResponse.getList().get(i).getDtTxt());
                         alltemp.add(bottomTemp);
-                        if (alltemp.size() > 5)
+                        if (alltemp.size() > 6)
                             break;
                     }
 
